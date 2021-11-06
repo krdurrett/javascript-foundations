a059167537de6cadc927d8d3c76d17114ec8edf6
Kayla Durrett <kayla.durrett@gmail.com>
2021-11-06 10:38:54 -0600

    Pass lunchbox tests

--- lunchbox/lunchbox.js
+++ lunchbox/lunchbox.js
@@ -1,5 +1,25 @@
 class LunchBox {
-
+  constructor(lunchbox) {
+    this.owner = lunchbox.owner;
+    this.material = lunchbox.madeOf;
+    this.shape = lunchbox.shape;
+    this.color = lunchbox.color;
+    this.snacks = [];
+  }
+  acquireSnack(snack) {
+    this.snacks.push(snack);
+    snack.isInLunchBox = true;
+  }
+  findHealthySnacks() {
+    var snacks = this.snacks;
+    var healthySnacks = [];
+    for (var i = 0; i < snacks.length; i ++) {
+      if (snacks[i].checkForHealthy()) {
+        healthySnacks.push(snacks[i].type);
+      }
+    }
+    return healthySnacks;
+  }
 }
 
 module.exports = LunchBox;
