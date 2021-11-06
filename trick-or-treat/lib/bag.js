df8dfe7a4e8fba28987c597f8b4a97dfd80f2f0d
Kayla Durrett <kayla.durrett@gmail.com>
2021-11-06 09:33:47 -0600

    Complete trick-or-treater

---
+++ trick-or-treat/lib/bag.js
@@ -0,0 +1,20 @@
+class Bag {
+  constructor() {
+    this.empty = true;
+    this.count = 0;
+    this.candies = [];
+  }
+  fill(candy) {
+    this.candies.push(candy);
+    this.count += 1;
+  }
+  contains(candy) {
+    if (this.candies[0].type.includes(candy)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+module.exports = Bag;
