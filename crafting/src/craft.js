1824ca9be1414542a05fb173f35d4193cde92d5a
Kayla Durrett <kayla.durrett@gmail.com>
2021-11-07 20:25:16 -0700

    Pass crafting

--- crafting/src/craft.js
+++ crafting/src/craft.js
@@ -1,5 +1,24 @@
 class Craft {
-
+  constructor(craft) {
+    this.name = craft.type;
+    this.materials = craft.materials;
+    this.completed = false;
+  }
+  completeCraft() {
+    this.completed = true;
+    return 'All done! It looks great!';
+  }
+  calculateProjectTotal() {
+    var cost = []
+    var totCost = 0;
+    for (var i = 0; i < this.materials.length; i++) {
+      cost.push(this.materials[i].price * this.materials[i].amount);
+    }
+    for (var i = 0; i < cost.length; i++) {
+      totCost += cost[i];
+    }
+    return totCost;
+  }
 }
 
 module.exports = Craft;
